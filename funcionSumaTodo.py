a2cb7f7f794e87d70a8735bf199b3ae7728318de
Claudia Sánchez <cisdheian@gmail.com>
2019-10-11 06:02:45 -0600

    funciones basicas

--- funcionSumaTodo.py
+++ funcionSumaTodo.py
@@ -4,4 +4,11 @@ def sumaTodos(limitTo):
         resultado += i
     return resultado
 
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(0, limitTo+1):
+        resultado += i*i
+    return resultado
+
 print (sumaTodos(100))
+print (sumaTodosLosCuadrados(100))
