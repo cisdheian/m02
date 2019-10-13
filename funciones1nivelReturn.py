371f26f403cfe76185d31f1146d2b1694551acce
Claudia Sánchez <cisdheian@gmail.com>
2019-10-12 19:52:55 -0600

    funciones 1er nivel return

---
+++ funciones1nivelReturn.py
@@ -0,0 +1,45 @@
+def maxi(*l):
+    if len(l) == 0 :
+        return 0
+    m = l[0]
+    for ix in range (1, len(l)):
+        if l[ix] > m:
+            m = l[ix]
+    return m
+
+def mini(*l):
+    if len(l) == 0 :
+        return 0
+    m = l[0]
+    for ix in range (1, len(l)):
+        if l[ix] < m:
+            m = l[ix]
+    return m
+
+def media(*l):
+    if len(l) == 0 :
+        return 0
+    suma =0 
+    for valor in l:
+            suma+=valor
+    return suma/len(l)
+
+funciones = {
+    'max':maxi,
+    'min':mini,
+    'med':media
+    }
+
+def returnF(nombre):
+    nombre = nombre.lower()
+    if nombre in funciones.keys():
+        return funciones[nombre]
+    return None
+
+
+
+print(maxi(1,2,-1,-15,9))
+print(mini(1,2,-1,-15,9))
+print(media(1,2,-1,-15,9))
+print(returnF('max'))
+print(returnF('max')(1,2,-1,-15,9))
